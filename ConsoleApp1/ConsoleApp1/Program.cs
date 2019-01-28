4cb3854b859b7377cc5136c69017ed7f0f7187a6
aamir <wasi3aamir@gmail.com>
2019-01-28 09:27:25 +0600

    Accepted

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            for(int t = 0; t < T; t++)
+            {
+                string line = Console.ReadLine();
+                if(line == "Easy Problem")
+                {
+                    Console.WriteLine(20);
+                }
+                else if (line == "Medium Problem")
+                {
+                    Console.WriteLine(40);
+                }
+                else if (line == "Hard Problem")
+                {
+                    Console.WriteLine(68);
+                }
+            }
+        }
+    }
+}
